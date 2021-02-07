df5d5052d9f51b1f4c0ae82bcb7e6a304a21a7f0
AmandaChristina <amanda.christina.n@gmail.com>
2021-02-06 21:16:30 -0300

    Subindo o projeto

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public double dinheiro;
+    public int habilidade;
+    public int dificuldade;
+
+
+
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 pos = new Vector3(xMove, yMove, 0);
+        transform.position += pos;
+
+        if(Input.GetKeyDown(KeyCode.F)){
+
+            dinheiro -= 20 * dificuldade;
+        }
+
+        else if(Input.GetKeyDown(KeyCode.G)){
+            habilidadeTest(dificuldade, habilidade);
+        }
+    }
+
+    int dado;
+    int somaTeste;
+
+     bool habilidadeTest(int dif, int hab){
+
+        bool testResultado;
+        dado = Random.Range(1,21);
+        somaTeste = dado+hab;
+
+        if(somaTeste >= dif*5){
+                print("sucesso");
+                return testResultado  = true;
+                
+
+        }
+
+        else{
+
+            dificuldade+=1;
+            print("fracasso");
+            return testResultado = false;
+            
+        }
+
+    }
+
+
+}
+
+
+
+
+    
---
+++ Assets/Scripts/Selecao.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecao : MonoBehaviour
+{
+    Camera cam;
+    public GameObject balao;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0)){
+
+            if(Physics.Raycast(ray, out hit)){
+                Debug.DrawRay (ray.origin, ray.direction * hit.distance, Color.red);
+                
+                if(hit.transform.gameObject.name == "pudim"){
+                    balao.SetActive(true);
+                }
+
+                else{              
+                    balao.SetActive(false);
+
+                }
+            }
+
+        }
+        
+    }
+}
