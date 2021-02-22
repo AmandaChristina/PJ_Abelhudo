3a16a9ae5dba04c3eb36a64b4dfb379b72200970
Kaio Nunes <kaionunes38@gmail.com>
2021-02-22 19:28:09 -0300

    camera

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetToFollow;
+    void Update()
+    {
+        transform.position = new Vector3(      
+            Mathf.Clamp(targetToFollow.position.x, -22.5f, 22.5f),
+            Mathf.Clamp(targetToFollow.position.y, -5f, 5f),
+            transform.position.z);
+    }
+}
