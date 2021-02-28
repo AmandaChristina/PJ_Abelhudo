61dde45449b5d7ec6eb73d01fd0822794706cf80
AmandaChristina <amanda.christina.n@gmail.com>
2021-02-27 22:55:44 -0300

    Atualizando cenário

---
+++ Assets/Scripts/SpriteObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On
+    }
+
+}
